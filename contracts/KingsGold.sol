079fa92b1a735cb83889e1135a80079f85e6ed31
"AlfredNorthman" <"KingdomGold@protonmail.com">
2022-05-16 13:22:43 +0200

    Rename

--- contracts/KingsGold.sol
+++ contracts/KingsGold.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 /// @custom:security-contact KingdomGold@protonmail.com
-contract KingdomGold is ERC20, ERC20Burnable {
-    constructor() ERC20("KingdomGold", "KGG") {
-        _mint(msg.sender, 100000000 * 10 ** decimals());
+contract KingsGold is ERC20, ERC20Burnable {
+    constructor() ERC20("KingsGold", "KSG") {
+        _mint(msg.sender, 100000000 * 10 ** decimals()); // 100 mill
     }
 }
